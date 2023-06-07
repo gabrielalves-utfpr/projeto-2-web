b7ec8fb74b4d49826825a12395b89fce0c05ca4d
gabrielalves-utfpr <gabrielalvessantos@alunos.utfpr.edu.br>
2023-06-07 13:26:50 -0300

    diagram

--- model/supplier.js
+++ model/supplier.js
@@ -3,3 +3,20 @@
 *   id: int pkey
 *   nome: string
 */
+
+const { DataTypes } = require("sequelize")
+const sequelize = require("../helpers/db")
+
+const UserModel = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+})
