aeed0283ffc08e944390d058e2a2c052c4fdb581
gabrielalves-utfpr <gabrielalvessantos@alunos.utfpr.edu.br>
2023-06-15 18:46:07 -0300

    setting basic routes

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express()
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use('/login', require("./routes/login"))
+app.use('/user', require("./control/UserAPI"))
+app.use('/product', require("./control/ProductAPI"))
+app.use('/categorie', require("./control/CategorieAPI"))
+app.use('/supplier', require("./control/SupplierAPI"))
+app.use('/install', require("./control/InstallAPI"))
+
 app.listen(3000, () => {
     console.log('Working... http://localhost:3000')
 })
